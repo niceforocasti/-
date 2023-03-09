8155c406b127ae7f9f27783b41e170e6494efd5b
niceforocasti <118582345+niceforocasti@users.noreply.github.com>
2023-03-08 21:17:15 -0500

    Add files via upload

---
+++ PROC37-1_4-referencia-maestra2-main/js/Game.js
@@ -0,0 +1,37 @@
+class Game {
+  constructor() {}
+
+  getState(){ var gameStateRef=database.ref("gameState");
+   gameStateRef.on("value", function(data){ 
+   gameState=data.val();
+ });
+ }
+
+  update(state){
+  database.ref("/").update({
+  gameState:state
+  })
+  }
+
+  handleElements(){
+    form.hide();
+    form.titleImg.position(40,50);
+    form.titleImg.class("efectoTitulo");
+  }
+ play(){
+  this.handleElements();
+
+  drawSprite();
+ }
+
+
+
+
+
+
+  start() {
+    form = new Form();
+    form.display();
+    player = new Player();
+  }
+}
